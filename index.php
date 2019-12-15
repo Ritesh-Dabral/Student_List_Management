785ba152516157091e83db285bb0c344eafd021f
ritesh dabral <riteshdabral1@gmail.com>
2019-12-15 13:27:31 +0530

    header and boot strap added

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+	require_once('other/header.php'); //connecting with database
 	require_once('DB/DB.php'); //connecting with database
 	echo "landn";
 ?>
---
+++ other/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>STUDENT_MANAGEMENT</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html>
