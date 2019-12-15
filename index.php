fbc5cc891c9a08c2cddb015c3fd74b892e7323c0
ritesh dabral <riteshdabral1@gmail.com>
2019-12-15 22:50:45 +0530

    working sql

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php 
 	//require_once('other/header.php'); //connecting with database
-	require_once('other/user.php'); //connecting with database
+	require_once('user.php'); //connecting with database
 	
-	$new_user = new User();
-	//$new_user->runQuery('SELECT * FROM student');
+	setup_conn();
+	//$sql = "INSERT INTO student (first_name, last_name, class, stream) VALUES ('Ritesh','Dabral','12','Science')";
+	//$sql = "DELETE FROM student WHERE id=1";;
+	//delet($sql);
+	//insert($sql);
+	$sql = "SELECT first_name,last_name,class,stream FROM student";
+	show($sql);
+	
 ?>
---
+++ user.php
@@ -0,0 +1,71 @@
+<?php
+
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$db = 'student_management';
+	
+	$conn; //connection variable
+	
+	//setup_connection
+	function setup_conn()
+	{
+		global $host,$user,$pass,$db,$conn;
+			
+		$conn = new mysqli($host, $user, $pass, $db);
+		// Check connection
+		if (mysqli_connect_error())
+			die("Database connection failed: " . mysqli_connect_error());
+	}
+
+	//insert
+	function insert($sql)
+	{
+		global $conn;
+		
+		if ($conn->query($sql) === TRUE) 
+			echo "New record created successfully";
+		else
+			echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	
+	//delete
+	function delet($sql)
+	{
+		global $conn;
+		
+		if ($conn->query($sql) === TRUE) 
+			echo "Record Deleted";
+		else
+			echo "Error Deleting record: ". $conn->error;
+	}
+	
+	//update
+	function update($sql)
+	{
+		global $conn;
+		
+		if ($conn->query($sql) === TRUE) 
+			echo "Record Updated Successfully";
+		else
+			echo "Error updating record: ". $conn->error;
+	}	
+	
+	//view
+	function show($sql)
+	{
+		global $conn;
+		
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				echo "Name: " . $row["first_name"]. " " .$row["last_name"]."	Class: ". $row["class"]."	Stream: ".$row["stream"]."<br>";
+			}
+		} 
+		else
+			echo "0 results";
+	}
+
+
+?>
